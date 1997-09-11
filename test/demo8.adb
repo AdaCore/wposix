7603c6d405ce65a82412d51c787dd63c5b6420b8
Pascal Obry <obry@adacore.com>
1997-09-11 20:03:23 +0000

    add -largs argument.

--- test/demo8.adb
+++ test/demo8.adb
@@ -1,14 +1,10 @@
 
-with Ada.Text_IO;
-
 with POSIX;
 with POSIX_IO;
 with POSIX_Process_Primitives;
 with POSIX_Process_Identification;
 
 procedure Demo8 is
-
-   use Ada;
 
    package PPP renames POSIX_Process_Primitives;
    package PPI renames POSIX_Process_Identification;
@@ -20,22 +16,22 @@ procedure Demo8 is
 
 begin
 
-   Text_IO.Put_Line ("demo8");
-
    PPP.Open_Template (GMAKE_Template);
 
    PPP.Set_File_Action_To_Open (GMAKE_Template,
                                 POSIX_IO.Standard_Output,
-                                POSIX.To_POSIX_String ("stdout"),
+                                POSIX.To_POSIX_String ("nul"),
                                 POSIX_IO.Write_Only);
 
    PPP.Set_File_Action_To_Open (GMAKE_Template,
                                 POSIX_IO.Standard_Error,
-                                POSIX.To_POSIX_String ("stderr"),
+                                POSIX.To_POSIX_String ("nul"),
                                 POSIX_IO.Write_Only);
 
    POSIX.Append (GMAKE_Args, "-i");
    POSIX.Append (GMAKE_Args, "demo1");
+   POSIX.Append (GMAKE_Args, "-largs");
+   POSIX.Append (GMAKE_Args, "-ladvapi32");
 
    PPP.Start_Process
      (Child    => GMAKE_Id,
