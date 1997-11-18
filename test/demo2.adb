ab73a9d2bfd2e4aff5246031189c913c089e4f6a
Pascal Obry <obry@adacore.com>
1997-11-18 20:31:05 +0000

    add tests.

--- test/demo2.adb
+++ test/demo2.adb
@@ -1,11 +1,13 @@
 
 with Ada.Command_Line;
+with Ada.Exceptions;
 with Ada.Text_IO;
 with POSIX;
 with POSIX_Files;
 
 procedure Demo2 is
 
+   use Ada;
    use Ada.Command_Line;
    use Ada.Text_IO;
    use POSIX;
@@ -22,6 +24,36 @@ procedure Demo2 is
       new For_Every_Directory_Entry (Action => Display);
 
 begin
-   Display_Directory (To_POSIX_String ("*.c"));
-   Display_Directory (To_POSIX_String ("*.adb"));
+   Put_Line ("List directory .");
+   Display_Directory (To_POSIX_String ("."));
+
+   New_Line;
+   Put_Line ("List directory c:\temp");
+   Display_Directory (To_POSIX_String ("c:\temp"));
+
+   New_Line;
+   Put_Line ("List directory /tmp");
+   Display_Directory (To_POSIX_String ("/tmp"));
+
+   begin
+      New_Line;
+      Put_Line ("List directory /tmp2");
+      Display_Directory (To_POSIX_String ("/tmp2"));
+   exception
+      when E : others =>
+         Put_Line (Exceptions.Exception_Name (E) & " - " &
+                   Exceptions.Exception_Message (E) & " : " &
+                   POSIX.Image (POSIX.Get_Error_Code));
+   end;
+
+   begin
+      New_Line;
+      Put_Line ("List directory /tmp/toto");
+      Display_Directory (To_POSIX_String ("/tmp/toto"));
+   exception
+      when E : others =>
+         Put_Line (Exceptions.Exception_Name (E) & " - " &
+                   Exceptions.Exception_Message (E) & " : " &
+                   POSIX.Image (POSIX.Get_Error_Code));
+   end;
 end Demo2;
