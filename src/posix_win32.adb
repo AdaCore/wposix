5ec1f015f9ce02ebc6c239977092490d0205292d
Pascal Obry <obry@adacore.com>
1998-10-21 20:16:02 +0000

    add helper functions to implement Wait_For_Child_Process (all, group). [Add_Child] new function [Remove_Child] new function [Exist] new function [Wait] new function

--- src/posix_win32.adb
+++ src/posix_win32.adb
@@ -2,6 +2,9 @@
 --  $Id$
 
 with Ada.Exceptions;
+with Ada.Unchecked_Conversion;
+with Ada.Unchecked_Deallocation;
+with Interfaces.C;
 with Interfaces.C.Strings;
 
 with POSIX;
@@ -91,5 +94,197 @@ package body POSIX_Win32 is
          return False;
       end if;
    end Is_Executable;
+
+   --  process list
+
+   function Process_ID_To_PROCESS_INFORMATION is
+     new Ada.Unchecked_Conversion  (POSIX_Process_Identification.Process_ID,
+                                    Win32.Winbase.PROCESS_INFORMATION);
+
+   type P_List;
+   type P_List_Access is access P_List;
+
+   type P_List is
+      record
+         Process : PPI.Process_ID;
+         Next    : P_List_Access;
+      end record;
+
+   procedure Free is new Ada.Unchecked_Deallocation (P_List, P_List_Access);
+
+
+   protected Process_List is
+      procedure Add    (Child  : in     PPI.Process_ID);
+      procedure Remove (Child  : in     PPI.Process_ID);
+      function  Exist  (Child  : in     PPI.Process_ID) return Boolean;
+      procedure Wait   (Status :    out PPP.Termination_Status;
+                        Block  : in     Boolean);
+   private
+      Process   : P_List_Access := null;
+      N_Process : Natural := 0;
+   end Process_List;
+
+   protected body Process_List is
+
+      procedure Add    (Child  : in     PPI.Process_ID) is
+      begin
+         Process := new P_List'(Child, Next => Process);
+         N_Process := N_Process + 1;
+      end Add;
+
+      procedure Remove (Child  : in     PPI.Process_ID) is
+         use type PPI.Process_ID;
+         PLa, PLa_Prev : P_List_Access;
+      begin
+         PLa := Process;
+
+         Remove_Child :
+         while PLa /= null loop
+            if PLa.Process = Child then
+               if PLa = Process then
+                  Process := PLa.Next;
+               else
+                  PLa_Prev.Next := PLa.Next;
+               end if;
+               Free (PLa);
+               exit Remove_Child;
+            else
+               PLa_Prev := PLa;
+               PLa := PLa.Next;
+            end if;
+         end loop Remove_Child;
+         N_Process := N_Process - 1;
+      end Remove;
+
+      function  Exist  (Child  : in     PPI.Process_ID) return Boolean is
+         use type PPI.Process_ID;
+         PLa : P_List_Access := Process;
+      begin
+         Check_Child :
+         while PLa /= null loop
+            if PLa.Process = Child then
+               return True;
+            else
+               PLa := PLa.Next;
+            end if;
+         end loop Check_Child;
+         return False;
+      end Exist;
+
+      function Get_Process_ID (H : Win32.Winnt.HANDLE)
+                               return PPI.Process_ID
+      is
+         use type Win32.Winnt.Handle;
+         PLa, PLa_Prev : P_List_Access;
+      begin
+         PLa := Process;
+         while PLa /= null loop
+            if Process_ID_To_PROCESS_INFORMATION
+                 (PLa.Process).hProcess = H
+            then
+               return PLa.Process;
+            else
+               PLa_Prev := PLa;
+               PLa := PLa.Next;
+            end if;
+         end loop;
+         return PPI.Null_Process_ID;
+      end Get_Process_ID;
+
+      procedure Wait   (Status :    out PPP.Termination_Status;
+                        Block  : in     Boolean)
+      is
+
+         use type Interfaces.C.unsigned_long;
+
+         type Exit_Stat is mod 2 ** Integer'Size;
+         type Termination_Status is
+            record
+               Pid         :  PPI.Process_ID := PPI.Null_Process_ID;
+               Exit_Status :  Exit_Stat;
+            end record;
+
+         function Termination_Status_Conv is
+           new Ada.Unchecked_Conversion
+           (Termination_Status, PPP.Termination_Status);
+
+         type Handle_Set is array (1 .. N_Process) of
+           aliased Win32.Winnt.HANDLE;
+
+         Handles : aliased Handle_Set;
+         PLa     : P_List_Access := Process;
+
+         Retcode   : Win32.DWORD;
+         Ok        : Win32.BOOL;
+         Exit_Code : aliased Win32.DWORD;
+         H         : Win32.Winnt.HANDLE;
+         Child     : PPI.Process_ID;
+
+      begin
+         if N_Process = 0 then
+            Raise_Error ("No Child Process", POSIX.No_Child_Process);
+         end if;
+
+         for K in Handle_Set'Range loop
+            Handles (K) :=
+              Process_ID_To_PROCESS_INFORMATION (PLa.Process).hProcess;
+            PLa := PLa.Next;
+         end loop;
+
+         if Block then
+            Retcode := Win32.Winbase.WaitForMultipleObjects
+              (Interfaces.C.unsigned_long (N_Process),
+               Handles (1)'Unchecked_Access,
+               Win32.FALSE,
+               Win32.Winbase.INFINITE);
+         else
+            Retcode := Win32.Winbase.WaitForMultipleObjects
+              (Interfaces.C.unsigned_long (N_Process),
+               Handles (1)'Unchecked_Access,
+               Win32.FALSE,
+               0);
+            if Retcode = Win32.Winbase.WAIT_TIMEOUT then
+               Status := Termination_Status_Conv
+                 (Termination_Status'(PPI.Null_Process_ID, 0));
+               return;
+            end if;
+         end if;
+
+         H := Handles (Integer (Retcode - Win32.Winbase.WAIT_OBJECT_0 + 1));
+
+         Ok := Win32.Winbase.GetExitCodeProcess (H,
+                                                 Exit_Code'Unchecked_Access);
+
+         Ok := Win32.Winbase.CloseHandle (H);
+
+         Child := Get_Process_ID (H);
+
+         Status := Termination_Status_Conv
+           (Termination_Status'(Child, Exit_Stat (Exit_Code)));
+         Remove (Child);
+      end Wait;
+
+   end Process_List;
+
+   procedure Add_Child    (Child  : in     PPI.Process_ID) is
+   begin
+      Process_List.Add (Child);
+   end Add_Child;
+
+   procedure Remove_Child (Child  : in     PPI.Process_ID) is
+   begin
+      Process_List.Remove (Child);
+   end Remove_Child;
+
+   function  Exist  (Child  : in     PPI.Process_ID) return Boolean is
+   begin
+      return Process_List.Exist (Child);
+   end Exist;
+
+   procedure Wait         (Status :    out PPP.Termination_Status;
+                           Block  : in     Boolean) is
+   begin
+      Process_List.Wait (Status, Block);
+   end Wait;
 
 end POSIX_Win32;
--- src/posix_win32.ads
+++ src/posix_win32.ads
@@ -5,9 +5,16 @@ with System;
 with Win32;
 with Win32.Winbase;
 with Win32.Winnt;
+
 with POSIX;
+with POSIX_Process_Identification;
+with POSIX_Process_Primitives;
+
 
 package POSIX_Win32 is
+
+   package PPP renames POSIX_Process_Primitives;
+   package PPI renames POSIX_Process_Identification;
 
    use type Win32.INT;
 
@@ -31,5 +38,12 @@ package POSIX_Win32 is
 
    Null_Handle  : constant Win32.Winnt.HANDLE
      := Win32.Winnt.HANDLE (System.Null_Address);
+
+   --  helper functions for POSIX_Process_Primitives Wait_For_Child_Process
+   procedure Add_Child    (Child  : in     PPI.Process_ID);
+   procedure Remove_Child (Child  : in     PPI.Process_ID);
+   function  Exist        (Child  : in     PPI.Process_ID) return Boolean;
+   procedure Wait         (Status :    out PPP.Termination_Status;
+                           Block  : in     Boolean);
 
 end POSIX_Win32;
