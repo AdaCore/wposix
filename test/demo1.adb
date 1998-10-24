740e91abffe69ab8120d4c1779d0c8ac8d486fd6
Pascal Obry <obry@adacore.com>
1998-10-24 08:06:08 +0000

    add test for POSIX_File_Status.Is_Regular_File

--- test/demo1.adb
+++ test/demo1.adb
@@ -3,12 +3,14 @@ with Ada.Command_Line;
 with Ada.Text_IO;
 with POSIX;
 with POSIX_Files;
+with POSIX_File_Status;
 
 procedure Demo1 is
 
    use Ada;
    use POSIX;
    use POSIX_Files;
+   use POSIX_File_Status;
 
    Filename : constant String := Command_Line.Argument (1);
 
@@ -19,6 +21,12 @@ begin
       Text_IO.Put_Line (Filename & " is present");
    else
       Text_IO.Put_Line (Filename & " is not present");
+   end if;
+
+   if Is_Regular_File (Get_File_Status (To_POSIX_String (Filename))) then
+      Text_IO.Put_Line (Filename & " is a regular file");
+   else
+      Text_IO.Put_Line (Filename & " is not a regular file");
    end if;
 
    if Is_Directory (To_POSIX_String (Filename)) then
