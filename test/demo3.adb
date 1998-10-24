0cb093627353a904ad8341382f94e82524fe2b5a
Pascal Obry <obry@adacore.com>
1998-10-24 19:14:15 +0000

    add test for POSIX_Process_Times

--- test/demo3.adb
+++ test/demo3.adb
@@ -4,12 +4,25 @@
 with Ada.Text_IO;
 with POSIX_IO;
 with POSIX_Permissions;
+with POSIX_Process_Times;
 
 procedure Demo3 is
 
    use Ada.Text_IO;
    use POSIX_IO;
    use POSIX_Permissions;
+   use POSIX_Process_Times;
+
+   procedure Display_Process_Times is
+      PT : Process_Times := Get_Process_Times;
+   begin
+      Put_Line ("Elapsed_Real_Time_Of : " &
+                Tick_Count'Image (Elapsed_Real_Time_Of (PT)));
+      Put_Line ("User_CPU_Time_Of : " &
+                Tick_Count'Image (User_CPU_Time_Of (PT)));
+      Put_Line ("System_CPU_Time_Of : " &
+                Tick_Count'Image (System_CPU_Time_Of (PT)));
+   end Display_Process_Times;
 
    File : File_Descriptor;
 
@@ -19,4 +32,10 @@ begin
    Put_Line ("Is_A_Terminal : " & Boolean'Image (Is_A_Terminal (File)));
 
    Close (File);
+
+   New_Line;
+   Display_Process_Times;
+   delay 2.0;
+   New_Line;
+   Display_Process_Times;
 end Demo3;
