dec895b6e532d9acc378097934748b7c3fa33fad
Pascal Obry <obry@adacore.com>
2000-02-16 19:36:53 +0000

    Now this is a renaming of the corresponding POSIX. packages.

--- src/posix_permissions.ads
+++ src/posix_permissions.ads
@@ -1,54 +1,8 @@
 
 --  $Id$
 --  Author : Pascal Obry
---  pascal_obry@csi.com
+--  p.obry@wanadoo.fr
 
-package POSIX_Permissions is
+with POSIX.Permissions;
 
-   type Permission is
-      (Others_Execute, Others_Write, Others_Read,
-       Group_Execute, Group_Write, Group_Read,
-       Owner_Execute, Owner_Write, Owner_Read,
-       Set_Group_ID, Set_User_ID);
-
-   type Permission_Set is array (Permission) of Boolean;
-
-   Owner_Permission_Set : constant Permission_Set
-     := Permission_Set'(Owner_Read | Owner_Write | Owner_Execute => true,
-                        others => false);
-
-   Group_Permission_Set : constant Permission_Set
-     := Permission_Set'(Group_Read | Group_Write | Group_Execute => true,
-                        others => false);
-
-   Others_Permission_Set : constant Permission_Set
-     := Permission_Set'(Others_Read | Others_Write | Others_Execute => true,
-                        others => false);
-
-   Access_Permission_Set : constant Permission_Set
-     := Permission_Set'(Owner_Read  | Owner_Write  | Owner_Execute  => true,
-                        Group_Read  | Group_Write  | Group_Execute  => true,
-                        Others_Read | Others_Write | Others_Execute => true,
-                        others => false);
-
-   Set_Group_ID_Set : constant Permission_Set
-     := Permission_Set'(Set_Group_ID => true,
-                        others => false);
-
-   Set_User_ID_Set : constant Permission_Set
-     := Permission_Set'(Set_User_ID => true,
-                        others => false);
-
-   --  POSIX Permission-oriented operations
-
-   function Get_Allowed_Process_Permissions
-     return Permission_Set;
-
-   procedure Set_Allowed_Process_Permissions
-     (Permissions : in     Permission_Set);
-
-   procedure Set_Allowed_Process_Permissions
-     (Permissions : in     Permission_Set;
-      Old_Perms   :    out Permission_Set);
-
-end POSIX_Permissions;
+package POSIX_Permissions renames POSIX.Permissions;
