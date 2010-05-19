90ee7124d236476261db1d6d0f236dc48a52b52e
Pascal Obry <obry@adacore.com>
2010-05-19 14:20:39 +0000

    More ACL clean-up for the testsuite.
    Continued work for J324-017.

--- regtests/tests/0010_change_file_permissions/test.py
+++ regtests/tests/0010_change_file_permissions/test.py
@@ -7,6 +7,10 @@ USER=os.getenv("USER")
 def remove_acl(file):
     exec_cmd('icacls', [file, '/remove', 'Everyone'], output_file='acl.log')
     exec_cmd('icacls', [file, '/remove', 'None'], output_file='acl.log')
+    exec_cmd('icacls', [file, '/remove', 'cyg_server'], output_file='acl.log')
+    exec_cmd('icacls', [file, '/remove', 'SYSTEM'], output_file='acl.log')
+    exec_cmd('icacls', [file, '/remove', 'Administrators'],
+             output_file='acl.log')
     exec_cmd('icacls', [file, '/remove', USER], output_file='acl.log')
 
 def create(file):
