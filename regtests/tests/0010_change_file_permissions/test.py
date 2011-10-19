aa83a891d9d7272d62b2ddd67e016213317d3fb8
Pascal Obry <obry@adacore.com>
2011-10-19 14:20:12 +0200

    Ignore icacls error when removing permissions.
    Some users/groups could be missing on the system. We do not want
    to report errors on those cases. Should fix tests on Windows Vista.
    
    Continued work for KA12-006.

--- regtests/tests/0010_change_file_permissions/test.py
+++ regtests/tests/0010_change_file_permissions/test.py
@@ -5,13 +5,18 @@ import os
 USER=os.getenv("USER")
 
 def remove_acl(file):
-    exec_cmd('icacls', [file, '/remove', 'Everyone'], output_file='acl.log')
-    exec_cmd('icacls', [file, '/remove', 'None'], output_file='acl.log')
-    exec_cmd('icacls', [file, '/remove', 'cyg_server'], output_file='acl.log')
-    exec_cmd('icacls', [file, '/remove', 'SYSTEM'], output_file='acl.log')
+    exec_cmd('icacls', [file, '/remove', 'Everyone'],
+             output_file='acl.log', ignore_error=True)
+    exec_cmd('icacls', [file, '/remove', 'None'],
+             output_file='acl.log', ignore_error=True)
+    exec_cmd('icacls', [file, '/remove', 'cyg_server'],
+             output_file='acl.log', ignore_error=True)
+    exec_cmd('icacls', [file, '/remove', 'SYSTEM'],
+             output_file='acl.log', ignore_error=True)
     exec_cmd('icacls', [file, '/remove', 'Administrators'],
-             output_file='acl.log')
-    exec_cmd('icacls', [file, '/remove', USER], output_file='acl.log')
+             output_file='acl.log', ignore_error=True)
+    exec_cmd('icacls', [file, '/remove', USER],
+             output_file='acl.log', ignore_error=True)
 
 def create(file):
     f=open(file, 'w')
