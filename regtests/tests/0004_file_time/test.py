7eb03c34abb0222aa9481894ca86e9af7b2a0313
Pascal Obry <obry@adacore.com>
2015-02-18 10:38:31 +0100

    No check on hours to avoid TZ issues.
    Fix style of Python script.
    
    For O216-049.

--- regtests/tests/0004_file_time/test.py
+++ regtests/tests/0004_file_time/test.py
@@ -5,24 +5,27 @@ import time
 import calendar
 import datetime
 
+
 def check_n(name, current, expected, error=0):
-    v = int (current)
-    if abs (v - expected) <= error:
+    v = int(current)
+    if abs(v - expected) <= error:
         print "OK " + name
     else:
         print "NOK " + name + ": '" + str(expected) + "' != '" + current + "'"
+
 
 def check_date(name, lines, index, day, month, year, hour, minute, second=0):
     check_n(name+"-"+str(index), lines[index][:-1], day)
     check_n(name+"-"+str(index+1), lines[index+1][:-1], month)
     check_n(name+"-"+str(index+2), lines[index+2][:-1], year)
-    check_n(name+"-"+str(index+3), lines[index+3][:-1], hour, error=2)
+    if hour != 0:
+        check_n(name+"-"+str(index+3), lines[index+3][:-1], hour, error=2)
     check_n(name+"-"+str(index+4), lines[index+4][:-1], minute)
     if second != 0:
         check_n(name+"-"+str(index+5), lines[index+5][:-1], second, error=5)
 
-ts_access=time.strptime('03 Feb 2001 04:05', '%d %b %Y %H:%M')
-ts_mod=time.strptime('9 Sep 1967 10:33:01', '%d %b %Y %H:%M:%S')
+ts_access = time.strptime('03 Feb 2001 14:05', '%d %b %Y %H:%M')
+ts_mod = time.strptime('9 Sep 1967 10:33:01', '%d %b %Y %H:%M:%S')
 
 file = open('file.txt', 'w')
 file.close()
@@ -40,14 +43,14 @@ while now.second > 56:
 
 run('demo7', output_file='demo7.out')
 
-file = open ('demo7.out')
+file = open('demo7.out')
 lines = file.readlines()
 
-check_date ("now", lines, 1, now.day, now.month, now.year, now.hour,
-            now.minute, now.second)
+check_date("now", lines, 1, now.day, now.month, now.year, 0,
+           now.minute, now.second)
 
-check_date ("access", lines, 8, day=3, month=2, year=2001, hour=4, minute=5)
-check_date ("mod", lines, 15, day=9, month=9, year=1967, hour=10,
-            minute=33, second=1)
-check_date ("change", lines, 22, day=9, month=9, year=1967, hour=10,
-            minute=33, second=1)
+check_date("access", lines, 8, day=3, month=2, year=2001, hour=0, minute=5)
+check_date("mod", lines, 15, day=9, month=9, year=1967, hour=0,
+           minute=33, second=1)
+check_date("change", lines, 22, day=9, month=9, year=1967, hour=0,
+           minute=33, second=1)
