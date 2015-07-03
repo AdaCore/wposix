e14047f880bdc2a65ff453fef82521a44e74e6cf
Pascal Obry <obry@adacore.com>
2015-07-03 17:02:39 +0200

    Uninstall previous installation if present in NSIS script.
    Add some comments.
    
    Continued work for MA30-041.

--- install.cmd
+++ install.cmd
@@ -3,9 +3,18 @@
 if "%1" == "" goto errorconf
 
 set PREFIX=%1
+set S_GPR=%1\share\gpr\wposix.gpr
+
+rem ----------------------------------------------- UNINSTALL
+
+if exist %S_GPR% gprinstall --uninstall --prefix=%PREFIX% wposix.gpr
+
+rem ----------------------------------------------- BUILD
 
 gprbuild -j0 -p -XPRJ_BUILD=Release -XLIBRARY_TYPE=static --subdirs=static wposix.gpr
 gprbuild -j0 -p -XPRJ_BUILD=Release -XLIBRARY_TYPE=relocatable --subdirs=relocatable -XWIN32ADA_BUILD=relocatable wposix.gpr
+
+rem ----------------------------------------------- INSTALL
 
 gprinstall -p --prefix=%PREFIX% -XLIBRARY_TYPE=static --subdirs=static wposix.gpr
 gprinstall -p --prefix=%PREFIX% -XLIBRARY_TYPE=relocatable --subdirs=relocatable --build-name=relocatable -XWIN32ADA_BUILD=relocatable wposix.gpr
